998f6b8248a0d9fd296bf66115da84674bf07b41
maxpxtts <maxpetts9@gmail.com>
2021-10-26 15:20:00 +0100

    Initial Commit

---
+++ Shared/tistApp.swift
@@ -0,0 +1,17 @@
+//
+//  tistApp.swift
+//  Shared
+//
+//  Created by mp on 26/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct tistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
